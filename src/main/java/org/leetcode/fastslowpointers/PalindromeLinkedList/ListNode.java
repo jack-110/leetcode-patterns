4b5d47c46457e02f372728cff6ace14e0caef25a
chen yang <13990779978@163.com>
2023-11-08 12:12:56 +0800

    Palindrome LinkedList.

---
+++ src/main/java/org/leetcode/fastslowpointers/PalindromeLinkedList/ListNode.java
@@ -0,0 +1,10 @@
+package org.leetcode.fastslowpointers.PalindromeLinkedList;
+
+public class ListNode {
+    int val = 0;
+    ListNode next;
+
+    ListNode(int value) {
+        this.val = value;
+    }
+}
