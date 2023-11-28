ab82697e27a13ebbb94d388b694ea9afd53248c0
chen yang <13990779978@163.com>
2023-11-28 10:25:29 +0800

    merge intervals.

--- src/main/java/org/leetcode/MergeIntervals/MergeIntervals/Solution.java
+++ src/main/java/org/leetcode/MergeIntervals/MergeIntervals/Solution.java
@@ -1,13 +1,41 @@
 package org.leetcode.MergeIntervals.MergeIntervals;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
-    public List<Interval> merge(List<Interval> intervals) {
+    public static List<Interval> merge(List<Interval> intervals) {
         List<Interval> mergedIntervals = new LinkedList<Interval>();
+
+        intervals.sort(Comparator.comparingInt(a -> a.start));
+        Iterator<Interval> intervalItr = intervals.iterator();
+        Interval interval = intervalItr.next();
+        int start = interval.start;
+        int end = interval.end;
+        while (intervalItr.hasNext()) {
+            interval = intervalItr.next();
+            if (interval.start <= end) {
+                end = Math.max(interval.end, end);
+            } else {
+                mergedIntervals.add(new Interval(start, end));
+                start = interval.start;
+                end = interval.end;
+            }
+        }
+
+        mergedIntervals.add(new Interval(start, end));
          
         return mergedIntervals;
+    }
+
+    public static void main(String[] args) {
+        List<Interval> input = new ArrayList<Interval>();
+        input.add(new Interval(1, 4));
+        input.add(new Interval(2, 5));
+        input.add(new Interval(7, 9));
+        System.out.print("Merged intervals: ");
+        for (Interval interval : Solution.merge(input))
+            System.out.print("[" + interval.start + "," + interval.end + "] ");
+        System.out.println();
     }
 }
 
