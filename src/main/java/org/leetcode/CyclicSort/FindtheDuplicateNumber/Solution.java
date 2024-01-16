bbd9829f75a65f2ba9f974c0dd82ab29cf082361
chen yang <13990779978@163.com>
2024-01-16 16:45:46 +0800

    find dup nums.

---
+++ src/main/java/org/leetcode/CyclicSort/FindtheDuplicateNumber/Solution.java
@@ -0,0 +1,27 @@
+package org.leetcode.CyclicSort.FindtheDuplicateNumber;
+
+public class Solution {
+
+    public static int findNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int num = nums[i];
+            if (num != i + 1) {
+                if (num != nums[num - 1]) {
+                    nums[i] = nums[num - 1];
+                    nums[num - 1] = num;
+                } else {
+                    return num;
+                }
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 4, 4, 3, 2};
+        System.out.println(findNumber(nums));
+    }
+}
