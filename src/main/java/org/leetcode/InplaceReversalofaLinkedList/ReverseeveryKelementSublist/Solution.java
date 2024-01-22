e68eb2aedc6ac10181464adb67871428b70610f9
chen yang <13990779978@163.com>
2024-01-22 17:17:54 +0800

    Reverse each k SubList.

---
+++ src/main/java/org/leetcode/InplaceReversalofaLinkedList/ReverseeveryKelementSublist/Solution.java
@@ -0,0 +1,55 @@
+package org.leetcode.InplaceReversalofaLinkedList.ReverseeveryKelementSublist;
+
+class Solution {
+    public static ListNode reverse(ListNode head, int k) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+
+        ListNode previousNode = null;
+        ListNode currentNode = head;
+        ListNode nextNode = null;
+        while (currentNode != null) {
+            ListNode endOfPreviousKSubListNode = previousNode;
+            ListNode endOfKSubListNode = currentNode;
+            for (int i  = 0; i < k && currentNode != null; i++) {
+                nextNode = currentNode.next;
+                currentNode.next = previousNode;
+                previousNode = currentNode;
+                currentNode = nextNode;
+            }
+
+            if (endOfPreviousKSubListNode == null) {
+                head = previousNode;
+            } else {
+                endOfPreviousKSubListNode.next = previousNode;
+            }
+            endOfKSubListNode.next = currentNode;
+            previousNode = endOfKSubListNode;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
+        head.next.next.next.next.next.next.next = new ListNode(8);
+
+        ListNode reversed = reverse(head, 3);
+        System.out.println(reversed);
+    }
+}
+
+class ListNode {
+  int val = 0;
+  ListNode next;
+
+  ListNode(int value) {
+    this.val = value;
+  }
+}
