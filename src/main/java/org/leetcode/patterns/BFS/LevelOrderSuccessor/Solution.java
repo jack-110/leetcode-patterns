02e5ff7afbd2253f8dfc4bf7ebd2c71e82f7d98b
chen yang <13990779978@163.com>
2024-03-29 11:01:56 +0800

    Level Order Successor.

---
+++ src/main/java/org/leetcode/patterns/BFS/LevelOrderSuccessor/Solution.java
@@ -0,0 +1,51 @@
+package org.leetcode.patterns.BFS.LevelOrderSuccessor;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+
+    public static TreeNode findSuccessor(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode treeNode = queue.poll();
+                if (treeNode.val == key) {
+                    return queue.peek();
+                }
+                if (treeNode.left != null) {
+                    queue.offer(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.offer(treeNode.right);
+                }
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        TreeNode successor = findSuccessor(root, 2);
+        System.out.println(successor.val);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
