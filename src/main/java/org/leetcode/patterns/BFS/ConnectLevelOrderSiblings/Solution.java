ab065d1414b7981027799b1e0ba30ac39241ea67
chen yang <13990779978@163.com>
2024-03-29 11:20:48 +0800

    Connect Level Order Siblings.

---
+++ src/main/java/org/leetcode/patterns/BFS/ConnectLevelOrderSiblings/Solution.java
@@ -0,0 +1,56 @@
+package org.leetcode.patterns.BFS.ConnectLevelOrderSiblings;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+
+    public static TreeNode connect(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode treeNode = queue.poll();
+                if (i == levelSize - 1) {
+                    treeNode.next = null;
+                } else {
+                    treeNode.next = queue.peek();
+                }
+                if (treeNode.left != null) {
+                    queue.offer(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.offer(treeNode.right);
+                }
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        TreeNode treeNode = connect(root);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode next;
+
+        TreeNode(int x) {
+            val = x;
+            left = right = next = null;
+        }
+    }
+}
