3457daf98d96e325ce8ce4844358ec0cd6fafe63
chen yang <13990779978@163.com>
2024-04-18 23:47:06 +0800

    Simplify Path.

---
+++ src/main/java/org/leetcode/patterns/Stack/SimplifyPath/Solution.java
@@ -0,0 +1,13 @@
+package org.leetcode.patterns.Stack.SimplifyPath;
+
+
+public class Solution {
+
+    public static String simplifyPath(String path) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(simplifyPath("/ab/cd"));
+    }
+}
