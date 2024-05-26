9bc43ec45a95788086c38d968d70aa7fa6fd884d
chen yang <13990779978@163.com>
2024-05-26 19:39:07 +0800

    Binary Tree Path Sum.

--- src/main/java/org/leetcode/patterns/DFS/BinaryTreePathSum/Solution.java
+++ src/main/java/org/leetcode/patterns/DFS/BinaryTreePathSum/Solution.java
@@ -4,12 +4,7 @@ public class Solution {
 
     public static boolean hasPath(TreeNode root, int sum) {
         if (root == null) {
-            return false;
-        }
-
-        // the leaf node
-        if (root.val == sum && root.left == null && root.right == null) {
-            return true;
+            return sum == 0;
         }
 
         return hasPath(root.left, sum - root.val) || hasPath(root.right, sum - root.val);
